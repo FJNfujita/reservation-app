a785346a1d4ebd65f66a13f6b3b2667c08cf2c56
FJNfujita <e.fujita@fujinetworks.co.jp>
2020-09-16 17:22:55 +0900

    フロントとバックの結合

--- src/app/product/product-detail/product-detail.component.ts
+++ src/app/product/product-detail/product-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { products } from '../../products';
+import { ProductService } from '../shared/product.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -12,13 +12,22 @@ export class ProductDetailComponent implements OnInit {
   product: any;
   
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private productService: ProductService
   ) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
-      this.product = products[+params.get('productId')];
+     const productsObservable = this.productService.getProductById(params.get('productId'));
+     productsObservable.subscribe(
+      (data) => { 
+        this.product = data
+       },
+      (err) => { console.error('something wrong occurred: ' + err); }
+    )
     });
+
+    
   }
 
 }
