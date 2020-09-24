e17a9ad8a95c29d40f275c27828695506b1ccc8c
FJNfujita <e.fujita@fujinetworks.co.jp>
2020-09-24 09:23:54 +0900

    Authバックエンド作成

--- server/index.js
+++ server/index.js
@@ -1,16 +1,19 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const config = require('./config') //index.jsを指す
 const FakeDb = require('./fake-db')
 
 const productRoutes = require('./routes/products')
+const userRoutes = require('./routes/users')
+
 const path = require('path')
 
 mongoose.connect(config.DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
     // useFindAndModify: false,
-    // useCreateIndex: true
+  //useCreateIndex: true
 })
 .then(
     ()=> {
@@ -23,8 +26,10 @@ mongoose.connect(config.DB_URI, {
 )
 
 const app = express()
+app.use(bodyParser.json())
 
 app.use('/api/v1/products', productRoutes)
+app.use('/api/v1/users', userRoutes)
 
 // app.get('/products', function(req, res) {
 //     res.json({'sucess':true})
